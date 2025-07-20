da8cad9efaec5742e0d8fe12c1a57259b12aae8c
brkylmz999 <brkylmz999@gmail.com>
2025-07-20 14:01:58 +0000

    splash screen and import added

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,86 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 import 'pages/dashboard_page.dart';
 import 'pages/objectives_page.dart';
 import 'pages/vision_page.dart';
 import 'pages/info_page.dart';
 import 'pages/settings_page.dart';
+
+// --- Splash video widget (can be moved to its own file if you want) ---
+class SplashVideoScreen extends StatefulWidget {
+  final VoidCallback onFinish;
+  const SplashVideoScreen({super.key, required this.onFinish});
+
+  @override
+  State<SplashVideoScreen> createState() => _SplashVideoScreenState();
+}
+
+class _SplashVideoScreenState extends State<SplashVideoScreen> {
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset('assets/video/splash_intro.mp4')
+      ..initialize().then((_) {
+        setState(() {});
+        _controller.play();
+      });
+    _controller.setVolume(1.0);
+    _controller.setLooping(false);
+    _controller.addListener(_checkVideoEnd);
+  }
+
+  void _checkVideoEnd() {
+    if (_controller.value.position >= _controller.value.duration) {
+      widget.onFinish();
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.removeListener(_checkVideoEnd);
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _controller.value.isInitialized
+          ? GestureDetector(
+              onTap: widget.onFinish, // tap to skip
+              child: Stack(
+                fit: StackFit.expand,
+                children: [
+                  FittedBox(
+                    fit: BoxFit.cover,
+                    child: SizedBox(
+                      width: _controller.value.size.width,
+                      height: _controller.value.size.height,
+                      child: VideoPlayer(_controller),
+                    ),
+                  ),
+                  Positioned(
+                    top: 44,
+                    right: 20,
+                    child: SafeArea(
+                      child: IconButton(
+                        icon: const Icon(Icons.close_rounded, color: Colors.white, size: 30),
+                        onPressed: widget.onFinish,
+                        tooltip: 'Skip intro',
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          : const Center(child: CircularProgressIndicator()),
+    );
+  }
+}
+
+// --- Main app ---
 
 void main() {
   runApp(const LifeMaxxApp());
@@ -20,8 +97,28 @@ class LifeMaxxApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
-      home: const MainNavigationPage(),
+      home: const SplashVideoLauncher(),
     );
+  }
+}
+
+class SplashVideoLauncher extends StatefulWidget {
+  const SplashVideoLauncher({super.key});
+  @override
+  State<SplashVideoLauncher> createState() => _SplashVideoLauncherState();
+}
+
+class _SplashVideoLauncherState extends State<SplashVideoLauncher> {
+  bool _splashDone = false;
+  @override
+  Widget build(BuildContext context) {
+    return _splashDone
+        ? const MainNavigationPage()
+        : SplashVideoScreen(onFinish: () {
+            setState(() {
+              _splashDone = true;
+            });
+          });
   }
 }
 
